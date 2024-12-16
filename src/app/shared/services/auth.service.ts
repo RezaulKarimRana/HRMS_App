01ec3bcdfbbc6da2553bbac89db0345bc761d68c
rana <md.karim@summitcommunications.net>
2024-12-16 18:05:01 +0600

    registration

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor() { }
+  baseURL = 'http://localhost:5007/api';
+  constructor(private http: HttpClient) {}
+  createUser(formData: any) {
+    return this.http.post(this.baseURL + '/signup', formData);
+  }
 }
--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,22 +1,110 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../shared/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+import { FirstKeyPipe } from '../../shared/pipes/first-key.pipe';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, FirstKeyPipe],
   templateUrl: './registration.component.html',
   styles: ``,
 })
 export class RegistrationComponent implements OnInit {
   isSubmitted: boolean = false;
+  form!: FormGroup;
 
-  constructor(public fb: FormBuilder) {
-    const form = this.fb.group({
-      fullName: [''],
-    });
-  }
+  constructor(
+    private fb: FormBuilder,
+    private service: AuthService,
+    private toastr: ToastrService
+  ) {}
+
+  passwordMatchValidator: ValidatorFn = (control: AbstractControl): null => {
+    const password = control.get('password');
+    const confirmPassword = control.get('confirmPassword');
+
+    if (password && confirmPassword && password.value != confirmPassword.value)
+      confirmPassword?.setErrors({ passwordMismatch: true });
+    else confirmPassword?.setErrors(null);
+
+    return null;
+  };
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.createForm();
+  }
+  createForm() {
+    this.form = this.fb.group(
+      {
+        fullName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/),
+          ],
+        ],
+        confirmPassword: [''],
+      },
+      { validators: this.passwordMatchValidator }
+    );
+  }
+  hasDisplayableError(controlName: string): Boolean {
+    const control = this.form.get(controlName);
+    return (
+      Boolean(control?.invalid) &&
+      (this.isSubmitted || Boolean(control?.touched) || Boolean(control?.dirty))
+    );
+  }
+  onSubmit() {
+    this.isSubmitted = true;
+    if (this.form.valid) {
+      this.service.createUser(this.form.value).subscribe({
+        next: (res: any) => {
+          if (res.succeeded) {
+            this.form.reset();
+            this.isSubmitted = false;
+            this.toastr.success('New user created!', 'Registration Successful');
+          }
+        },
+        error: (err) => {
+          if (err.error.errors)
+            err.error.errors.forEach((x: any) => {
+              switch (x.code) {
+                case 'DuplicateUserName':
+                  break;
+
+                case 'DuplicateEmail':
+                  this.toastr.error(
+                    'Email is already taken.',
+                    'Registration Failed'
+                  );
+                  break;
+
+                default:
+                  this.toastr.error(
+                    'Contact the developer',
+                    'Registration Failed'
+                  );
+                  console.log(x);
+                  break;
+              }
+            });
+          else console.log('error:', err);
+        },
+      });
+    }
   }
 }
